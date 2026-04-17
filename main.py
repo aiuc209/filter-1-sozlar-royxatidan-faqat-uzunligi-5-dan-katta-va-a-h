bfbeb7a8afe369e3e0d0e9cf59052bc8588dbb95
aiuc209 <dzuiryuomdbaoy@gmail.com>
2026-04-17 22:59:56 +0500

    🚀 Add main application code

---
+++ main.py
@@ -0,0 +1,11 @@
+"""
+Filter 1 Sozlar Royxatidan Faqat Uzunligi 5 Dan Katta Va A H
+Topic: FILTER – 1: ��So‘zlar ro‘yxatidan faqat uzunligi 5 dan katta va a harfini o‘z ichiga olgan so‘zlargina ajratib  olinadigan filter + lambda funksiyasini yozing.
+"""
+
+def main():
+    print("Welcome to Filter 1 Sozlar Royxatidan Faqat Uzunligi 5 Dan Katta Va A H!")
+    print("Muvaffaqiyatli ishga tushirildi.")
+
+if __name__ == "__main__":
+    main()
